24f65c5644cc9771839a65c251b518d9f616b7eb
apoorvarajan <apoorvarajan1997@gmail.com>
2023-06-08 13:29:08 -0400

    loc change

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -159,7 +159,7 @@ const Home = (props:any) =>{
                         </div>
                     </div>
                     <div className="photo-wrap">
-                        <img className="photo" height="250em" src="../headshot.png"/>
+                        <img className="photo" height="250em" src="./headshot.png"/>
                     </div>
                 </div>
                 <div className="about-me">
