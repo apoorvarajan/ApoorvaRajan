3318a809bb59be7cfae31a9c22baea823beca61d
apoorvarajan <apoorvarajan1997@gmail.com>
2023-07-02 19:37:03 -0400

    ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import ReactGA from 'react-ga';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home'
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-P88RFPWDVG');
+    ReactGA.pageview(window.location.pathname + window.location.search)
+    },[]);
   return (
     <div className="App">
       <Home/>
