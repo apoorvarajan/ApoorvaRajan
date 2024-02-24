da15080330b0e87536d48c5ea5bd3d35265e82ac
apoorvarajan <apoorvarajan1997@gmail.com>
2024-02-24 15:44:22 -0500

    anothr try

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ function App() {
     <div className="App">
       <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/playground" element={<PlayGround />} />
+        <Route path="/" Component={Home} />
+        <Route path="/playground" Component={PlayGround} />
       </Routes>
       </HashRouter>
     </div>
